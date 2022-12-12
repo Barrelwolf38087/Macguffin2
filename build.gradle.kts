34450c30d5551a3dd6b66b9bb7a978277e232469
Willow Leverone <Barrelwolf38087@gmail.com>
2022-12-12 14:59:35 -0500

    Update Paper to 1.19.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ repositories {
 
 dependencies {
     @Suppress("VulnerableLibrariesLocal")
-    compileOnly("io.papermc.paper:paper-api:1.19.2-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.19.3-R0.1-SNAPSHOT")
 }
 
 java {
